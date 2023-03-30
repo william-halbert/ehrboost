0a24cb399be7d295b0215b46587d8a524987147d
William Halbert <willhalbert16@gmail.com>
2023-03-30 17:48:09 +1100

    heroku-v1

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 const app = require("./app");
-const port = process.env.PORT;
+
+app.get("*", () => {});
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
